0bc131837f02dbe1d6fdd13f27d35479ffba5374
sjwilczynski <sjwilczynski@gmail.com>
2020-09-12 21:52:39 +0200

    more debugging

--- api/authorization/firebaseAuthorization.ts
+++ api/authorization/firebaseAuthorization.ts
@@ -1,10 +1,17 @@
-import admin from "../firebase/firebase";
+import admin, { newConfig } from "../firebase/firebase";
 
 export const firebaseGetUserId = async (token: string) => {
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
     return decodedToken.uid;
   } catch (err) {
-    throw new Error("Unable to identify the user. " + err.message +" " + process.env.serviceWorkerId);
+    throw new Error(
+      "Unable to identify the user. " +
+        err.message +
+        " Token:" +
+        token +
+        " " +
+        newConfig
+    );
   }
 };
