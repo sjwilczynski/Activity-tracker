fd194af3caedfc0a978cfe0fa0f50fefd215c8f4
sjwilczynski <sjwilczynski@gmail.com>
2020-10-12 21:37:07 +0200

    use table from material

--- client/src/components/table/SummaryTable.tsx
+++ client/src/components/table/SummaryTable.tsx
@@ -1,5 +1,14 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@material-ui/core";
 import * as React from "react";
-import { Table } from "react-bootstrap";
 import { ActivityRecordWithId } from "../../data";
 
 type Props = {
@@ -8,25 +17,54 @@ type Props = {
 
 export function SummaryTable(props: Props) {
   const { records } = props;
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const changePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const changeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   if (!records) {
     return <></>;
   }
   return (
-    <Table responsive striped bordered hover>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Activity name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {records.map((record) => (
-          <tr key={record.id}>
-            <td>{record.date.toLocaleDateString("en-CA")}</td>
-            <td>{record.name}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+    <Paper>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Activity name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {records
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((record) => (
+                <TableRow key={record.id}>
+                  <TableCell>
+                    {record.date.toLocaleDateString("en-CA")}
+                  </TableCell>
+                  <TableCell>{record.name}</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 20, 50, 100]}
+        component="div"
+        count={records.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={changePage}
+        onChangeRowsPerPage={changeRowsPerPage}
+      />
+    </Paper>
   );
 }
