5c32b504642e747d16af6004dffb1db50d3d08f4
sjwilczynski <sjwilczynski@gmail.com>
2020-12-13 15:54:32 +0100

    style the login screen

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -1,6 +1,7 @@
 import { FirebaseAuth } from "react-firebaseui";
 import firebase from "firebase/app";
 import "firebase/auth";
+import { makeStyles } from "@material-ui/core";
 
 const uiConfig: firebaseui.auth.Config = {
   // Popup signin flow rather than redirect flow.
@@ -15,11 +16,39 @@ const uiConfig: firebaseui.auth.Config = {
   },
 };
 
+const useStyles = makeStyles((theme) => {
+  return {
+    container: {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      padding: "2rem",
+    },
+    text: {
+      ...theme.typography.h3,
+      display: "flex",
+      [theme.breakpoints.down("xs")]: {
+        flexFlow: "column wrap",
+        alignItems: "center",
+      },
+    },
+    active: { color: theme.palette.primary.main },
+    lazy: { color: theme.palette.error.main },
+    or: {
+      margin: "0 0.5rem",
+    },
+  };
+});
+
 export const Login = () => {
+  const styles = useStyles();
   return (
-    <div>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <span className={styles.active}>Team active</span>
+        <span className={styles.or}>or</span>
+        <span className={styles.lazy}>Team lazy</span>
+      </div>
       <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </div>
   );
