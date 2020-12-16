5587cc7ee57a8731e54f3eefd1a7849c363b3dce
sjwilczynski <sjwilczynski@gmail.com>
2020-12-16 22:40:23 +0100

    fix types for optimistic updates

--- client/src/data/hooks/useDeleteAllActivities.ts
+++ client/src/data/hooks/useDeleteAllActivities.ts
@@ -6,28 +6,35 @@ import {
   getActivitiesQueryId,
 } from "../react-query-config/query-constants";
 import { ActivityRecordWithId } from "../types";
+import { ActivityMutationContext } from "./useActivitiesMutation";
 
 export const useDeleteAllActivities = () => {
   const client = useQueryClient();
   const deleteAllActivities = useDeleteAllActivitiesFunction();
-  return useMutation<void, Error, {}, () => void>(deleteAllActivities, {
-    onMutate: () => {
-      client.cancelQueries(getActivitiesQueryId, { exact: true });
+  return useMutation<void, Error, {}, ActivityMutationContext>(
+    deleteAllActivities,
+    {
+      onMutate: () => {
+        client.cancelQueries(getActivitiesQueryId, { exact: true });
 
-      const previousActivityRecords = client.getQueryData<
-        ActivityRecordWithId[]
-      >(getActivitiesQueryId);
-      client.setQueryData<ActivityRecordWithId[], Error>(
-        getActivitiesQueryId,
-        []
-      );
-      return () =>
-        client.setQueryData(getActivitiesQueryId, previousActivityRecords);
-    },
-    onError: (err, newTodo, rollback) => rollback(),
-    onSettled: () =>
-      client.invalidateQueries(getActivitiesQueryId, { exact: true }),
-  });
+        const previousActivityRecords =
+          client.getQueryData<ActivityRecordWithId[]>(getActivitiesQueryId) ||
+          [];
+        client.setQueryData<ActivityRecordWithId[]>(getActivitiesQueryId, []);
+        return { previousActivityRecords };
+      },
+      onError: (error, variables, context) => {
+        if (context) {
+          client.setQueryData(
+            getActivitiesQueryId,
+            context.previousActivityRecords
+          );
+        }
+      },
+      onSettled: () =>
+        client.invalidateQueries(getActivitiesQueryId, { exact: true }),
+    }
+  );
 };
 
 const useDeleteAllActivitiesFunction = () => {
