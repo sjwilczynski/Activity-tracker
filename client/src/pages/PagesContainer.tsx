99deb219d9756bce85f83892c91da7e07510e8d2
Stanisław <sjwilczynski@gmail.com>
2023-01-07 22:38:19 +0100

    move sections down on mobile

--- client/src/pages/PagesContainer.tsx
+++ client/src/pages/PagesContainer.tsx
@@ -4,12 +4,16 @@ type Props = {
   children: React.ReactNode;
 };
 
-const Container = styled("div")({
+const Container = styled("div")(({ theme }) => ({
   padding: "4rem 2rem",
   flexDirection: "column",
   display: "flex",
   gridArea: "content",
-});
+
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "center",
+  },
+}));
 
 export const PagesContainer = ({ children }: Props) => (
   <Container>{children}</Container>
