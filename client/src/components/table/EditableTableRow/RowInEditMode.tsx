ced7bce36073f04363e0302c9c0ecfe50e6e4522
Stanisław <sjwilczynski@gmail.com>
2023-03-26 18:54:08 +0200

    show somehitng for now

--- client/src/components/table/EditableTableRow/RowInEditMode.tsx
+++ client/src/components/table/EditableTableRow/RowInEditMode.tsx
@@ -11,6 +11,8 @@ export const RowInEditMode = (props: Props) => {
   console.log(props.record);
   return (
     <TableRow>
+      <TableCell>{props.record.date.toLocaleDateString("en-CA")}</TableCell>
+      <TableCell>{props.record.name}</TableCell>
       <TableCell>
         <Button startIcon={<CancelIcon />} onClick={props.onCancel}>
           Cancel
