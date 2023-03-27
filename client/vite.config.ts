cc47ceb0f55f11491cab9684dfa8208737c314db
Stanisław <sjwilczynski@gmail.com>
2023-03-28 00:34:58 +0200

    update lockfile and config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,10 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  build: {
+    outDir: "build",
+  },
+  server: {
+    open: true,
+  },
 });
