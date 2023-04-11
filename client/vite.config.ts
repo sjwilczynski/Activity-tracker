2f2e52f841e9f01522a9f35783611c1d00d82efd
Stanisław <sjwilczynski@gmail.com>
2023-04-11 21:14:59 +0200

    use vite plugin to get service worker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,50 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Activity tracker",
+        short_name: "AT",
+        lang: "en",
+        description: "A place to track and review all your activities",
+        start_url: ".",
+        background_color: "#f2f2f2",
+        theme_color: "#4479a2",
+        dir: "ltr",
+        display: "standalone",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   base: "/",
   build: {
     outDir: "build",
